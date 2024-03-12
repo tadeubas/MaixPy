ea61696314e0f579436c7b482773ab319fc0669d
odudex <eduardo.schoenknecht@gmail.com>
2024-03-12 19:34:49 -0300

    fix Yahboom hardcoded SD card IO mapping

--- projects/maixpy_yahboom/main/src/main.cpp
+++ projects/maixpy_yahboom/main/src/main.cpp
@@ -5,7 +5,7 @@ extern sdcard_config_t config;
 
 int main()
 {
-    sdcard_config_t amigo = { 10, 6, 11, 26, SD_CS_PIN };
+    sdcard_config_t amigo = { 35, 33, 32, 34, SD_CS_PIN };
     config = amigo;
     maixpy_main();
     return 0;
