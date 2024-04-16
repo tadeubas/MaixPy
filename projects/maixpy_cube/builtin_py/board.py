8f3773c9806a385e597fea86d174581f1ad3a157
odudex <eduardo.schoenknecht@gmail.com>
2024-04-16 15:30:03 -0300

    add Cube backlight GPIO pin

--- projects/maixpy_cube/builtin_py/board.py
+++ projects/maixpy_cube/builtin_py/board.py
@@ -34,6 +34,7 @@ config = json.loads("""
             "BUTTON_C": 11,
             "I2C_SDA": 31,
             "I2C_SCL": 30
+            "BACKLIGHT": 17,
         },
         "display": {
             "touch": false,
