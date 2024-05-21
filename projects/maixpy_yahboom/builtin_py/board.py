2a45cd81f8017ec2965bfe2bf1a0b0048a412789
tadeubas <tadeubas@gmail.com>
2024-05-21 13:54:24 -0300

    Remove unnecessary configs "inverted_coordinates" and "qr_colors"

--- projects/maixpy_yahboom/builtin_py/board.py
+++ projects/maixpy_yahboom/builtin_py/board.py
@@ -42,9 +42,7 @@ config = json.loads("""
         },
         "display": {
             "touch": true,
-            "font": [8, 16],
-            "inverted_coordinates": false,
-            "qr_colors": [0, 6342]
+            "font": [8, 16]
         }
     }
 }
