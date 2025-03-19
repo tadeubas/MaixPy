aec46ec759ccbe4a8febd4df0db079dae85b127b
odudex <odudex@proton.me>
2025-03-19 09:13:05 -0300

    hardware accelerated pbkdf2_hmac

--- components/micropython/port/src/moduhashlib_maixpy.c
+++ components/micropython/port/src/moduhashlib_maixpy.c
@@ -21,6 +21,9 @@
 #if MICROPY_PY_UHASHLIB_MAIX
 #include "sha256.h"
 
+#define SHA256_BASE_ADDR    (0x502C0000U)
+#define SHA256_BLOCK_LEN   64L
+
 typedef struct _mp_obj_hash_t {
     mp_obj_base_t base;
     size_t total_len;
@@ -28,6 +31,16 @@ typedef struct _mp_obj_hash_t {
 } mp_obj_hash_t;
 
 STATIC const mp_obj_type_t uhashlib_sha256_type;
+
+volatile sha256_t* const sha256_hw = (volatile sha256_t*)SHA256_BASE_ADDR;
+
+void sha256_quick_init(sha256_context_t *context, size_t input_len)
+{
+    sha256_hw->sha_num_reg.sha_data_cnt = (uint32_t)((input_len + SHA256_BLOCK_LEN + 8) / SHA256_BLOCK_LEN);
+    sha256_hw->sha_function_reg_0.sha_en = 0x1;
+    context->total_len = 0L;
+    context->buffer_len = 0L;
+}
 
 #if MICROPY_PY_UHASHLIB_SHA256_MAIX
 STATIC mp_obj_t uhashlib_sha256_update(mp_obj_t self_in, mp_obj_t arg);
@@ -71,9 +84,140 @@ STATIC mp_obj_t uhashlib_sha256_hard(mp_obj_t self_in,mp_obj_t arg) {
     return mp_obj_new_str_from_vstr(&mp_type_bytes, &vstr);
 }
 
+STATIC mp_obj_t mod_uhashlib_pbkdf2_hmac_sha256(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    enum { ARG_password, ARG_salt, ARG_iterations, ARG_dklen };
+    static const mp_arg_t allowed_args[] = {
+        { MP_QSTR_password, MP_ARG_OBJ | MP_ARG_REQUIRED, {.u_obj = MP_OBJ_NULL} },
+        { MP_QSTR_salt, MP_ARG_OBJ | MP_ARG_REQUIRED, {.u_obj = MP_OBJ_NULL} },
+        { MP_QSTR_iterations, MP_ARG_INT | MP_ARG_REQUIRED, {.u_int = 0} },
+        { MP_QSTR_dklen, MP_ARG_INT | MP_ARG_INT, {.u_int = 32} },
+    };
+
+    // uint64_t start_us = sysctl_get_time_us();
+
+    mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
+    mp_arg_parse_all(n_args, pos_args, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
+
+    // Get password and salt as buffers
+    mp_buffer_info_t password_buf, salt_buf;
+    mp_get_buffer_raise(args[ARG_password].u_obj, &password_buf, MP_BUFFER_READ);
+    mp_get_buffer_raise(args[ARG_salt].u_obj, &salt_buf, MP_BUFFER_READ);
+
+    int iterations = args[ARG_iterations].u_int;
+    int dklen = args[ARG_dklen].u_int;
+
+    if (iterations < 1) {
+        mp_raise_ValueError("iterations must be >= 1");
+    }
+    if (dklen < 1) {
+        mp_raise_ValueError("dklen must be >= 1");
+    }
+
+    // Process the password into the key (64 bytes)
+    uint8_t key[64];
+    if (password_buf.len > 64) {
+        // Hash the password using incremental functions for hardware acceleration
+        sha256_context_t key_ctx;
+        sha256_init(&key_ctx, 64);
+        sha256_update(&key_ctx, password_buf.buf, password_buf.len);
+        uint8_t hashed_key[32];
+        sha256_final(&key_ctx, hashed_key);
+        memcpy(key, hashed_key, 32);
+        memset(key + 32, 0, 32);
+    } else {
+        memcpy(key, password_buf.buf, password_buf.len);
+        memset(key + password_buf.len, 0, 64 - password_buf.len);
+    }
+
+    // Precompute HMAC pads
+    uint8_t inner_pad[64], outer_pad[64];
+    for (int i = 0; i < 64; i++) {
+        inner_pad[i] = key[i] ^ 0x36;
+        outer_pad[i] = key[i] ^ 0x5C;
+    }
+
+    int hlen = 32;
+    int l = (dklen + hlen - 1) / hlen;
+    vstr_t dk_vstr;
+    vstr_init_len(&dk_vstr, l * hlen);
+    uint8_t *dk = (uint8_t *)dk_vstr.buf;
+
+    for (int i = 1; i <= l; i++) {
+        // Create initial message: salt || i (BE)
+        uint8_t initial_message[salt_buf.len + 4];
+        memcpy(initial_message, salt_buf.buf, salt_buf.len);
+        initial_message[salt_buf.len] = (i >> 24) & 0xFF;
+        initial_message[salt_buf.len + 1] = (i >> 16) & 0xFF;
+        initial_message[salt_buf.len + 2] = (i >> 8) & 0xFF;
+        initial_message[salt_buf.len + 3] = i & 0xFF;
+
+        // Compute U_1 = HMAC(inner_pad || salt_plus_i)
+        uint8_t u_current[32];
+        {
+            // Inner hash
+            sha256_context_t inner_ctx, outer_ctx;
+            sha256_quick_init(&inner_ctx, 64);
+            size_t total_len = 64;
+            sha256_update_length(total_len);
+            sha256_update(&inner_ctx, inner_pad, 64);
+            total_len += salt_buf.len + 4;
+            sha256_update_length(total_len);
+            sha256_update(&inner_ctx, initial_message, salt_buf.len + 4);
+            uint8_t inner_hash[32];
+            sha256_final(&inner_ctx, inner_hash);
+
+            // Outer hash
+            // sha256_init(&outer_ctx, 64);
+            sha256_quick_init(&outer_ctx, 64);
+            sha256_update(&outer_ctx, outer_pad, 64); // Fixed pad
+            sha256_update(&outer_ctx, inner_hash, 32); // Varying data
+            sha256_final(&outer_ctx, u_current);
+        }
+
+        // Initialize accum with U_1
+        uint8_t accum[32];
+        memcpy(accum, u_current, 32);
+
+        // Iterations loop (j starts at 2)
+        for (int j = 2; j <= iterations; j++) {
+            // Compute U_j = HMAC(u_prev)
+
+            sha256_context_t inner_ctx;
+            sha256_quick_init(&inner_ctx, 64);
+            sha256_update(&inner_ctx, inner_pad, 64); // Fixed pad
+            sha256_update(&inner_ctx, u_current, 32); // Varying data
+            uint8_t inner_hash_j[32];
+            sha256_final(&inner_ctx, inner_hash_j);
+
+            sha256_context_t outer_ctx;
+            sha256_quick_init(&outer_ctx, 64);
+            sha256_update(&outer_ctx, outer_pad, 64); // Fixed pad
+            sha256_update(&outer_ctx, inner_hash_j, 32); // Varying data
+            sha256_final(&outer_ctx, u_current);
+
+            // XOR accum with u_current (32-bit optimized)
+            uint32_t *accum32 = (uint32_t*)accum;
+            uint32_t *u32 = (uint32_t*)u_current;
+            for (int k = 0; k < 8; k++) {
+                accum32[k] ^= u32[k];
+            }
+        }
+
+        // Copy accum to derived key
+        int offset = (i - 1) * 32;
+        int copy_len = (i == l) ? dklen - offset : 32;
+        memcpy(dk + offset, accum, copy_len);
+    }
+    // uint64_t end_us = sysctl_get_time_us();
+    // mp_printf(&mp_plat_print, "PBKDF2 took %lu us\n", (unsigned long)(end_us - start_us));
+    
+    return mp_obj_new_bytes(dk, dklen);
+}
+
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(uhashlib_sha256_update_obj, uhashlib_sha256_update);
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(uhashlib_sha256_digest_obj, uhashlib_sha256_digest);
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(uhashlib_sha256_hard_obj, uhashlib_sha256_hard);
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(pbkdf2_hmac_sha256_obj, 3, mod_uhashlib_pbkdf2_hmac_sha256);
 
 STATIC const mp_rom_map_elem_t uhashlib_sha256_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_update), MP_ROM_PTR(&uhashlib_sha256_update_obj) },
@@ -94,6 +238,7 @@ STATIC const mp_obj_type_t uhashlib_sha256_type = {
 STATIC const mp_rom_map_elem_t mp_module_uhashlib_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_uhashlib) },
     { MP_ROM_QSTR(MP_QSTR_sha256), MP_ROM_PTR(&uhashlib_sha256_type) },
+    { MP_ROM_QSTR(MP_QSTR_pbkdf2_hmac_sha256), MP_ROM_PTR(&pbkdf2_hmac_sha256_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(mp_module_uhashlib_globals, mp_module_uhashlib_globals_table);
